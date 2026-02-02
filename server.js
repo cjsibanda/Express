15145652a6d5af60737c4204a081125a2df32a38
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-02-02 03:07:11 +0000

    Add views and public folders

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello Mr. Sibanda :)");
+const path = require("path");
+const express = require('express');
+const app = express(); 
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.listen(PORT, () => {
-    console.log("Server listening on port " + PORT);
-})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send("Something is Off!")
+});
+
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+
+app.listen(HTTP_PORT, onHttpStart);
