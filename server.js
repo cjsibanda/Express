94ba41249e14babe8718d52075702b463d7b9299
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-02-25 00:40:00 +0000

    modified server.js

--- server.js
+++ server.js
@@ -1,42 +1,74 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
-const employeeUtil = require("./modules/player-util");
-
+const express = require("express");
 const app = express();
-
-// Set up EJS
-app.set("view engine", "ejs");
-app.set("layout", "layouts/main");
-app.use(expressLayouts);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get('/', (req, res) => {
-    res.render("home", {
-        players: playerUtil.getAllPlayers(),
-        title: "Player List" 
-    });
-});
-
-//
-app.get('/visible', (req, res) => {
-    res.render("home", {
-        employees: playerUtil.getVisibleEmployees(playerUtil.getAllEmployees()),
-        title: "Active Players Only"
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.render("about", {
-        title: "About"
-    });
-});
+const mongoose = require('mongoose');
 
 const HTTP_PORT = process.env.PORT || 8080;
+const DB = `Full MongoDB Connection String Here`;
 
-app.listen(HTTP_PORT, () => {
-    console.log(`server listening on: ${HTTP_PORT}`);
+app.set("view engine", "ejs");
+
+let Schema = mongoose.Schema;
+
+const nameSchema = new Schema({
+  fName: String,
+  lName: String
 });
 
+let Name = mongoose.model('names', nameSchema);
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+
+  Name.find().sort({createdAt: 1}).exec().then((data) => {
+      res.render("home", { data });
+  }).catch(err=>{
+    console.log(err);
+  });
+
+});
+
+app.post("/updateName", (req, res) => {
+    if (req.body.lName.length == 0 && req.body.fName.length == 0) {
+
+        Name.deleteOne({_id: req.body._id}).exec().then(() => {
+            console.log("successfully removed name: " + req.body._id);
+            res.redirect("/"); 
+        });
+
+    } else {
+
+      Name.updateOne({_id: req.body._id}, {
+        $set: {
+          lName: req.body.lName,
+          fName: req.body.fName
+        }
+      }).exec().then(() => {
+          console.log("successfully updated name: " + req.body._id);
+          res.redirect("/");
+      });
+
+    }
+});
+
+app.post("/addName", (req, res) => {
+
+  const newName = new Name({
+    lName: req.body.lName,
+    fName: req.body.fName
+  });
+
+  newName.save().then(() => {
+      console.log(" created a new name");
+      res.redirect("/");
+  });
+});
+
+mongoose.connect(DB).then(()=>{
+  app.listen(HTTP_PORT, ()=>{
+    console.log(`server listening on: ${HTTP_PORT}`);
+  });
+}).catch(err=>{
+  console.log(err);
+})
